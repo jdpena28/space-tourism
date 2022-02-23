0b1bfb31c6af2f16c590d1cbfc033e3e1e39f468
jdpena28 <johnhenrich1128@gmail.com>
2022-02-23 21:28:46 +0800

    Setup typeface & pallette colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,11 @@ import { NextSeo } from "next-seo";
 
 const Home: NextPage = () => {
   return (
-    <section className="flex h-screen w-full items-center justify-center">
+    <section className="flex h-screen w-full items-center justify-center font-primary">
       <NextSeo
-        title="Next.js Boilerplate"
+        title="Space Tourism Website"
         description="Next.js Boilerplate configuration with Eslint, Prettier and Husky"
       />
-      <div className="text-center">
-        <h1 className="text-3xl font-semibold">Next.js Boilerplate</h1> <br />
-        <p>Configuration with Eslint, Prettier and Husky</p>
-        <ul className="list-disc !text-left">
-          <li className="">
-            <a href="https://github.com/tailwindlabs/prettier-plugin-tailwindcss">
-              Tailwind Uniformed Class Order
-            </a>
-          </li>
-          <li>
-            <a href="https://typescript-eslint.io/docs/linting/">
-              TS Lint Parser
-            </a>
-          </li>
-        </ul>
-      </div>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,41 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: ["Bellefair", "serif"],
+      secondary: ["Barlow Condensed", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#0B0D17",
+        highlight: "D0D6F9",
+      },
+      fontSize: {
+        heading1: "150px",
+        heading2: "100px",
+        heading3: "56px",
+        heading4: "32px",
+        heading5: [
+          "28px",
+          {
+            letterSpacing: "4.75px",
+          },
+        ],
+        subheading1: "28px",
+        subheading2: [
+          "14px",
+          {
+            letterSpacing: "2.35px",
+          },
+        ],
+        navText: [
+          "16px",
+          {
+            letterSpacing: "2.7px",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
