aa15af5d02a9e1953c1ffc8d1b090ca363494326
jdpena28 <johnhenrich1128@gmail.com>
2022-02-27 10:45:58 +0800

    add indicator component

---
+++ src/components/Indicator.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Indicator = () => {
+  return (
+    <div className="mt-24 flex gap-x-4">
+      <div className="h-4 w-4 cursor-pointer rounded-full bg-white" />
+      <div className="h-4 w-4 cursor-pointer rounded-full bg-gray-500" />
+      <div className="h-4 w-4 cursor-pointer rounded-full bg-gray-500" />
+      <div className="h-4 w-4 cursor-pointer rounded-full bg-gray-500" />
+    </div>
+  );
+};
+
+export default Indicator;
