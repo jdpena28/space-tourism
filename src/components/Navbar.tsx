8b5feba5c7c788dd6eb8d6bb6145067bb271a404
jdpena28 <johnhenrich1128@gmail.com>
2022-03-05 13:29:33 +0800

    BUGFIX: active route on index not working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { route } from "next/dist/server/router";
 
-const TextLink: React.FC<TextLinkProps> = ({
-  navCount,
-  navText,
-  navLink,
-  isActive,
-}) => {
-  const { asPath, pathname } = useRouter();
-  useEffect(() => {
-    console.log(pathname);
-  }, []);
-
+const TextLink: React.FC<TextLinkProps> = ({ navCount, navText, navLink }) => {
+  const { pathname } = useRouter();
   return (
     // eslint-disable-next-line @next/next/link-passhref
     <Link href={navLink}>
@@ -31,11 +22,8 @@ const TextLink: React.FC<TextLinkProps> = ({
 };
 
 const Navbar = () => {
+  const { asPath } = useRouter();
   const data = [
-    {
-      text: "HOME",
-      link: "/",
-    },
     {
       text: "DESTINATION",
       link: "/destination/moon",
@@ -54,6 +42,15 @@ const Navbar = () => {
       <Image src="/assets/shared/logo.svg" height={48} width={48} />
       <div className="z-30 -mr-40 h-[1px]  w-[40%] bg-[#979797] lg:hidden" />
       <div className="z-20 flex w-[50%] justify-center gap-x-6 bg-white/10 backdrop-blur-md lg:w-max lg:pl-4 lg:text-sm">
+        <Link href="/">
+          <div
+            className={`cursor-pointer space-x-2 border-b-2 border-transparent py-4  hover:border-gray-400 ${
+              asPath === "/" ? "border-white" : ""
+            }`}>
+            <span className="font-bold">00</span>
+            <span className="text-navText font-thin">HOME</span>
+          </div>
+        </Link>
         {data.map((item, count) => {
           return (
             <TextLink
