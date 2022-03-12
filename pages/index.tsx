7f4a6f086df1ab9165b72ef8a464b9cd1d94e9a3
jdpena28 <johnhenrich1128@gmail.com>
2022-03-12 18:36:55 +0800

    FEATURE: index page responsive for all breakpoints

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,27 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <Layout id="home" className="flex h-full items-center">
-      <div className="fixed bottom-8 flex w-full items-center justify-around lg:static lg:mt-8 lg:h-max lg:flex-col  lg:gap-y-20 lg:text-center">
+    <Layout id="home">
+      <div className="container m-auto flex h-full flex-col justify-around py-2 pt-16 text-center lg:fixed lg:bottom-9 lg:left-0 lg:right-0 lg:h-max lg:flex-row lg:items-center">
         <div>
           <p className="font-secondary text-heading5  text-highlight">
             SO, YOU WANT TO TRAVEL TO
           </p>
-          <h1 className="text-heading1 ">SPACE</h1>
-          <p className="w-[24.6rem] font-tertiary text-highlight lg:w-[27rem]">
+          <h1 className="text-7xl md:text-9xl">SPACE</h1>
+          <p className="mx-auto w-[80%] font-tertiary text-highlight lg:w-[27rem]">
             Let&apos;s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we&apos;ll give you a truly out of
             this world experience!
           </p>
         </div>
-        <Link href="destination/moon">
-          <div className="h-72 w-72 cursor-pointer rounded-full bg-white text-center leading-[18rem] ring-gray-400/50 transition duration-300 ease-in-out hover:ring-[40px]">
-            <span className="text-heading4 text-black">EXPLORE</span>
-          </div>
-        </Link>
+        <div>
+          <Link href="destination/moon">
+            <div className="mx-auto h-36 w-36 cursor-pointer rounded-full bg-white text-center leading-[9rem] ring-gray-400/50 transition duration-300 ease-in-out hover:ring-[40px] md:h-52 md:w-52 md:leading-[13rem]">
+              <span className="text-xl text-black">EXPLORE</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </Layout>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { nanoid } from "nanoid";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
-import { route } from "next/dist/server/router";
 
 const TextLink: React.FC<TextLinkProps> = ({ navCount, navText, navLink }) => {
   const { pathname } = useRouter();
@@ -11,10 +10,10 @@ const TextLink: React.FC<TextLinkProps> = ({ navCount, navText, navLink }) => {
     // eslint-disable-next-line @next/next/link-passhref
     <Link href={navLink}>
       <div
-        className={`cursor-pointer space-x-2 border-b-2 border-transparent py-4  hover:border-gray-400  ${
+        className={`cursor-pointer space-x-2 border-b-2 border-transparent py-7  hover:border-gray-400  ${
           pathname.includes(navText.toLowerCase()) ? "border-white" : ""
         }`}>
-        <span className="font-bold">{navCount}</span>
+        <span className="hidden font-bold lg:inline">{navCount}</span>
         <span className="text-navText font-thin">{navText}</span>
       </div>
     </Link>
@@ -38,16 +37,25 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="absolute flex w-full items-center justify-between pt-3 pl-6 font-secondary">
+    <nav className="fixed top-0 flex w-full items-center justify-between p-5">
       <Image src="/assets/shared/logo.svg" height={48} width={48} />
-      <div className="z-30 -mr-40 h-[1px]  w-[40%] bg-[#979797] lg:hidden" />
-      <div className="z-20 flex w-[50%] justify-center gap-x-6 bg-white/10 backdrop-blur-md lg:w-max lg:pl-4 lg:text-sm">
+      {/*  mobile menu */}
+      <svg
+        className="cursor-pointer fill-current text-highlight md:hidden"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24">
+        <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+      </svg>
+      <div className="absolute  right-0 top-0 z-20  hidden w-max  justify-center gap-x-6 bg-white/10 px-6 backdrop-blur-md md:flex lg:static">
+        <div className="absolute top-0 bottom-0 -left-[97%] z-30 m-auto hidden  h-[1px] w-full bg-[#979797] lg:block" />
         <Link href="/">
           <div
-            className={`cursor-pointer space-x-2 border-b-2 border-transparent py-4  hover:border-gray-400 ${
+            className={`cursor-pointer space-x-2 border-b-2 border-transparent py-7  hover:border-gray-400 ${
               asPath === "/" ? "border-white" : ""
             }`}>
-            <span className="font-bold">00</span>
+            <span className="hidden font-bold lg:inline">00</span>
             <span className="text-navText font-thin">HOME</span>
           </div>
         </Link>
