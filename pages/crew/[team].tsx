fbc93708755cbe1811153c29930b9a4a79d4d554
jdpena28 <johnhenrich1128@gmail.com>
2022-03-17 11:36:42 +0800

    BUGFIX: issue with nextjs/image fit to content

--- pages/crew/[team].tsx
+++ pages/crew/[team].tsx
@@ -27,19 +27,19 @@ const Team = () => {
           text="MEET YOUR CREW"
           className="text-center md:ml-12 md:text-left lg:fixed lg:top-[20%] lg:ml-0"
         />
-        <div className="items-center md:flex md:flex-col-reverse md:gap-y-20 lg:flex-row-reverse ">
-          <div className="mx-auto h-[271px] w-[216px] overflow-y-hidden md:-mb-32 md:h-[572px] md:w-[456.37px] lg:-mb-8 lg:h-[70%] lg:w-[70%]">
+        <div className="items-center md:flex md:flex-col-reverse  md:gap-y-20 lg:flex-row-reverse">
+          <div className="relative mx-auto h-[39vh] w-full md:h-[59vh]  lg:-mb-14 lg:h-[80vh] 2xl:-mb-44">
             <Image
               src={
                 datas
                   ? datas.images.webp
                   : "/assets/crew/image-douglas-hurley.webp"
               }
-              width={datas ? datas.images.width : 568.07}
-              height={datas ? datas.images.height : 712}
+              layout="fill"
+              objectFit="contain"
             />
           </div>
-          <div className="mt-5 flex flex-col  gap-y-5">
+          <div className="mt-5 flex flex-col  gap-y-5 lg:w-full">
             <Indicator />
             <div className="md:order-1 lg:space-y-5">
               <p className="uppercase text-slate-400 md:text-2xl">
@@ -49,7 +49,7 @@ const Team = () => {
                 {datas?.name}
               </h3>
             </div>
-            <p className="mx-auto w-[80%] text-center font-tertiary text-highlight md:order-2 md:w-[60%] lg:m-0 lg:text-left">
+            <p className="mx-auto w-[80%] text-center font-tertiary text-highlight md:order-2 md:w-[60%]  lg:m-0 lg:text-left">
               {datas?.bio}
             </p>
           </div>
